d995fa627d0a4896c0d42fd9efb8ae4ee18cb79f
Roland Walker <walker@pobox.com>
2012-10-17 17:35:56 -0400

    test todos

--- ert-tests/fixmee-test.el
+++ ert-tests/fixmee-test.el
@@ -14,6 +14,104 @@
 
 ;;; this is a stub - no tests defined
 
+;;; todo - fixmee-refresh-timer-setup (&optional arg)
+
+;;; todo - fixmee-cache-invalidate ()
+
+;;; todo - fixmee-get-buffer-state () ;; optimization
+
+;;; todo - fixmee-this-buffer-not-pristine-hook (&optional beg end len)
+
+;;; todo - fixmee-buffer-included-p (buf)
+
+;;; todo - fixmee-measure-urgency (str-val)
+
+;;; todo - fixmee-sort-notice-list ()
+
+;;; todo - fixmee-notices-from-pristine-buffers () ;; optimization
+
+;;; todo - fixmee-notices-from-current-buffer ()
+
+;;; todo - fixmee-locate-all-notices ()
+
+;;; todo - fixmee-inside-notice-p (&optional pos)
+
+;;; todo - fixmee-leave-current-notice (&optional reverse)
+
+;;; todo - fixmee-navigate-to-hit (hit)
+
+;;; todo - fixmee-find-nextmost-urgent (hit)
+
+;;; todo - fixmee-find-prevmost-urgent (hit)
+
+;;; todo - fixmee-find-next-by-position (&optional pos)
+
+;;; todo - fixmee-find-previous-by-position (&optional pos)
+
+;;; todo - fixmee-notice-popup (ev)
+
+;;; todo - fixmee-button-setup (&optional arg)
+
+;;; todo - fixmee--listview-name-sorter (a b)
+
+;;; todo - fixmee--listview-urgency-sorter (a b)
+
+;;; todo - fixmee--listview-context-sorter (a b)
+
+;;; todo - fixmee--listview-max-line-number-width (notice-list)
+
+;;; todo - fixmee--listview-buffer-plus-line-formatted (buf pos len)
+
+;;; todo - fixmee--listview-render-entries (notice-list)
+
+;;; todo - fixmee--listview-find-current-notice ()
+
+;;; todo - fixmee--listview-print-entry-function (id cols)
+
+;;; todo - fixmee--listview-revert-function (&optional _ignore1 _ignore2)
+
+;;; todo - fixmee-maybe-turn-on (&optional arg)
+
+;;; todo - fixmee-mode-maybe-global-teardown ()
+
+;;; todo - fixmee-reload ()
+
+;;; todo - fixmee-goto-nextmost-urgent (&optional arg)
+
+;;; todo - fixmee-goto-prevmost-urgent (&optional arg)
+
+;;; todo - fixmee-goto-next-by-position (&optional arg)
+
+;;; todo - fixmee-goto-previous-by-position (&optional arg)
+
+;;; todo - fixmee-mouse-goto-nextmost-urgent (event)
+
+;;; todo - fixmee-mouse-goto-prevmost-urgent (event)
+
+;;; todo - fixmee-mouse-goto-next-by-position (event)
+
+;;; todo - fixmee-mouse-goto-previous-by-position (event)
+
+;;; todo - fixmee-listview-goto-notice ()
+
+;;; todo - fixmee--listview-next-error-function (&optional arg reset)
+
+;;; todo - fixmee-listview-view-notice ()
+
+;;; todo - fixmee-show-listview (&optional arg)
+
+;;; todo - fixmee-listview-toggle-local-only ()
+
+;;; todo - fixmee-listview-previous-error (&optional arg)
+
+;;; todo - fixmee-listview-next-buffer (&optional arg)
+
+;;; todo - fixmee-listview-previous-buffer (&optional arg)
+
+;;; todo - fixmee-listview-mouse-goto-notice (event)
+
+;;; todo - fixmee-listview-quit (&optional buf)
+
 ;;
 ;; Emacs
 ;;
