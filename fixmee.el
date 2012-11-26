ef69078606432c66d4fb2fbcd155da750239ac2f
Roland Walker <walker@pobox.com>
2012-11-26 12:27:44 -0500

    improve compat of called-interactively-p macro including trunk (to be 24.3) and versions with only `interactive-p`

--- fixmee.el
+++ fixmee.el
@@ -775,9 +775,15 @@ Expressed as an element of `fixmee-notice-list'.")
 
 Optional KIND is as documented at `called-interactively-p'
 in GNU Emacs 24.1 or higher."
-  (if (eq 0 (cdr (subr-arity (symbol-function 'called-interactively-p))))
-      '(called-interactively-p)
-    `(called-interactively-p ,kind)))
+  (cond
+    ((not (fboundp 'called-interactively-p))
+     '(interactive-p))
+    ((condition-case nil
+         (progn (called-interactively-p 'any) t)
+       (error nil))
+     `(called-interactively-p ,kind))
+    (t
+     '(called-interactively-p))))
 
 ;;; compatibility functions
 
